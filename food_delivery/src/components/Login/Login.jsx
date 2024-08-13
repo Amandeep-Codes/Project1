0320927e37930d7e97c8686429f35a682f9f1f82
Amandeep-codes <reachamandeep8@gmail.com>
2024-08-13 17:08:37 +0530

    finished styling Login/Sign up

--- food_delivery/src/components/Login/Login.jsx
+++ food_delivery/src/components/Login/Login.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../../assets/assets";
+import './Login.css'
 
-const Login = () => {
+const Login = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Login");
   return (
-    <div className='login-popup'>
-      
-    </div>
-  )
-}
+    <div className="login-popup">
+      <form action="" className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt="icon"
+          />
+        </div>
+        <div className="login-popup-input">
+            {currentState=== "Login" ? <></> : <input type="text" placeholder="Your name" required/>}
+            <input type="email" placeholder="Your email" required/>
+            <input type="password" placeholder="Password" required />
+        </div>
+        <button> {currentState === "Sign Up" ? "Create account" : "Login "} </button>
+        <div className="login-popup-condition">
+            <input type="checkbox" required />
+            <p>By continuing , i agree to the term and condition of use and privacy policy</p>
 
-export default Login
+        </div>
+        {currentState==="Login" ? <p> Create a new account? <span onClick={()=>setCurrentState("Sign Up")}> Click here</span></p> : <p> Already have an account? <span onClick={()=>setCurrentState("Login")}> Login here</span></p>}
+        
+        
+      </form>
+    </div>
+  );
+};
+
+export default Login;
