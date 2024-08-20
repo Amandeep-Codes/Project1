6d3b6cc60317c3102dc99cfc6dc9f04d0b2393a8
Amandeep-codes <reachamandeep8@gmail.com>
2024-08-20 12:06:08 +0530

    adding components

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,9 +1,17 @@
 
 import './App.css';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import Income from './components/Income';
 
 function App() {
   return (
    <>
+   <Header/>
+   <div className="container">
+    <Balance/>
+    <Income/>
+   </div>
    </>
   );
 }
